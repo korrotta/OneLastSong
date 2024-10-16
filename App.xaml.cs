154da865f684da3ef34a0b75ed07834b649518a0
lhson0606 <lehoangson01633892497@gmail.com>
2024-10-16 20:04:49 +0700

    added Styles & Themes folder

--- App.xaml.cs
+++ App.xaml.cs
@@ -42,8 +42,11 @@ namespace OneLastSong
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            //this line is for testing purposes only
+            ThemeUtils.ChangeTheme(ThemeUtils.LIGHT_THEME);
+            //ThemeUtils.LoadStoredTheme(); uncomment this line to load the stored theme
+
             _window = new MainWindow();
-            ThemeUtils.LoadStoredTheme();
             _window.Activate();
         }
 
