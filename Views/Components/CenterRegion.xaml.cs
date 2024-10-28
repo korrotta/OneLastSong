28419858301d7432d2f16d8ec363ccb10a6e0e59
lhson0606 <lehoangson01633892497@gmail.com>
2024-10-28 20:54:53 +0700

    add playlist and song list examples in Center Page

--- Views/Components/CenterRegion.xaml.cs
+++ Views/Components/CenterRegion.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using OneLastSong.Models;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +27,28 @@ namespace OneLastSong.Views.Components
         public CenterRegion()
         {
             this.InitializeComponent();
+            StyledGrid.ItemsSource = GenerateRandomData(10);
+            ExampleList.ItemsSource = GenerateRandomData(10);
+        }
+
+        private List<CustomDataObject> GenerateRandomData(int count)
+        {
+            var random = new Random();
+            var dataList = new List<CustomDataObject>();
+
+            for (int i = 0; i < count; i++)
+            {
+                dataList.Add(new CustomDataObject
+                {
+                    Title = $"Title {i + 1}",
+                    ImageLocation = "/Assets/LikedPlaylist.png",
+                    Views = random.Next(0, 10000).ToString(),
+                    Likes = random.Next(0, 5000).ToString(),
+                    Description = $"Description for item {i + 1}"
+                });
+            }
+
+            return dataList;
         }
     }
 }
