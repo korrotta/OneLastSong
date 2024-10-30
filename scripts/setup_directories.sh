b74bed03a5de07b42ab723cd0926f165174bba1c
lhson0606 <lehoangson01633892497@gmail.com>
2024-10-30 22:50:46 +0700

    add setup directories script for Windows & MacOS

---
+++ scripts/setup_directories.sh
@@ -0,0 +1,15 @@
+cd ../postgres_bind_folder
+mkdir -p pg_commit_ts
+mkdir -p pg_dynshmem
+mkdir -p pg_logical/mappings
+mkdir -p pg_logical/snapshots
+mkdir -p snapshots
+mkdir -p pg_notify
+mkdir -p pg_replslot
+mkdir -p pg_serial
+mkdir -p pg_snapshots
+mkdir -p pg_stat_tmp
+mkdir -p pg_tblspc
+mkdir -p pg_twophase
+mkdir -p pg_wal/archive_status
+mkdir -p pg_wal/summaries
