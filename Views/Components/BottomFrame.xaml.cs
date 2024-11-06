3328c83256f34dd0cf999a5573835dad2b4520d5
korrotta <hoaihieulam@gmail.com>
2024-11-06 21:10:50 +0700

    Added MediaPlayer

--- Views/Components/BottomFrame.xaml.cs
+++ Views/Components/BottomFrame.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.UI.Xaml.Controls;
 using OneLastSong.ViewModels;
+using System;
 
 namespace OneLastSong.Views.Components
 {
@@ -12,6 +13,40 @@ namespace OneLastSong.Views.Components
             this.InitializeComponent();
             this.ViewModel = new BottomFrameViewModel();
             this.DataContext = this.ViewModel;
+
+            ViewModel.LoadSong(new Uri("https://vgmsite.com/soundtracks/minecraft/kvptjmornx/1-18.%20Sweden.mp3"));
+        }
+
+        private void PlaybackButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            ViewModel.PlayPause();
+            PlaybackIcon.Glyph = ViewModel.IsPlaying ? "\uE769" : "\uE768";
+        }
+
+        private void RepeatButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProgressBar_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            ProgressBar.Maximum = ViewModel.TotalTime.TotalSeconds;
+        }
+
+        private void ProgressBar_Moved(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+
+        }
+
+
+        private void VolumeSlider_ValueChanged(object sender, Microsoft.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            ViewModel.SetVolume(e.NewValue / 100);
+        }
+
+        private void Fullscreen_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+
         }
     }
 }
